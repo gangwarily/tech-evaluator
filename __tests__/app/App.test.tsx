ea3393f2585aeded681eb381ddf2e27214e0a7dc
Stuart Drummond <stuartrdr@gmail.com>
2019-01-12 20:15:07 -0800

    TypeScript typing + clean up

--- __tests__/app/App.test.tsx
+++ __tests__/app/App.test.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import App from '../../src/js/app/App';
-import TestRenderer from 'react-test-renderer';
+import TestRenderer, {ReactTestInstance, ReactTestRenderer} from 'react-test-renderer';
 
 describe('App component unit test', () => {
     it('render test', () => {
-        const renderer = TestRenderer.create(<App />);
-        const root = renderer.root;
+        const renderer: ReactTestRenderer = TestRenderer.create(<App />);
+        const root: ReactTestInstance = renderer.root;
 
         expect(root).not.toBeNull();
         expect(renderer.toJSON()).toMatchSnapshot();
 
-        const div = root.findByType('div');
+        const div: ReactTestInstance = root.findByType('div');
         expect(root.findAllByType('div').length).toEqual(1);
 
-        const span = div.findByType('span');
+        const span: ReactTestInstance = div.findByType('span');
         expect(root.findAllByType('span').length).toEqual(1);
         expect(span.children[0]).toEqual('Hello!');
     })
