b1ee3eb18d51a9c4baa7597148b7faec0f251d0f
Stuart Drummond <stuartrdr@gmail.com>
2019-02-18 20:45:30 -0800

    Implemented the transition animation for modals and added comments

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
 import App, { SomeType } from './app/App';
 import { hello } from './utils';
+
+ReactModal.setAppElement('#app');
 
 const type: SomeType = {
     foo: 'bar',
--- src/js/modal/BootstrapModal.tsx
+++ src/js/modal/BootstrapModal.tsx
@@ -15,6 +15,11 @@ const BootstrapModal = () => {
 
     const save = () => {};
 
+    // By Using a form library like Formik, you can defer all the state management to it. Formik also includes a resetForm function.
+    // You can wait until submission is done before closing the component.
+    // Component state is being completely driven by this component
+    // By implementing onRequestClose, you can close modal with esc or clicking on overlay with no additional implementation
+
     return (
         <div>
             <button type="button" className="btn btn-danger" onClick={onClick}>
@@ -22,25 +27,61 @@ const BootstrapModal = () => {
             </button>
 
             <ReactModal
-                className="Modal__Bootstrap modal-dialog"
+                className="modal-dialog"
                 isOpen={isOpen}
                 closeTimeoutMS={150}
+                shouldCloseOnOverlayClick
+                shouldCloseOnEsc
+                onRequestClose={() => setIsOpen(false)}
             >
                 <div className="modal-content">
                     <div className="modal-header">
                         <h4 className="modal-title">Modal title</h4>
-                        <button type="button" className="close" onClick={onClick}>
+                        <button
+                            type="button"
+                            className="close"
+                            onClick={onClick}
+                        >
                             <span aria-hidden="true">&times;</span>
                             <span className="sr-only">Close</span>
                         </button>
                     </div>
                     <div className="modal-body">
                         <h4>Really long content...</h4>
-                        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                        <p>
+                            Pellentesque habitant morbi tristique senectus et
+                            netus et malesuada fames ac turpis egestas.
+                            Vestibulum tortor quam, feugiat vitae, ultricies
+                            eget, tempor sit amet, ante. Donec eu libero sit
+                            amet quam egestas semper. Aenean ultricies mi vitae
+                            est. Mauris placerat eleifend leo. Quisque sit amet
+                            est et sapien ullamcorper pharetra. Vestibulum erat
+                            wisi, condimentum sed, commodo vitae, ornare sit
+                            amet, wisi. Aenean fermentum, elit eget tincidunt
+                            condimentum, eros ipsum rutrum orci, sagittis tempus
+                            lacus enim ac dui. Donec non enim in turpis pulvinar
+                            facilisis. Ut felis. Praesent dapibus, neque id
+                            cursus faucibus, tortor neque egestas augue, eu
+                            vulputate magna eros eu erat. Aliquam erat volutpat.
+                            Nam dui mi, tincidunt quis, accumsan porttitor,
+                            facilisis luctus, metus
+                        </p>
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" onClick={onClick}>Close</button>
-                        <button type="button" className="btn btn-primary" onClick={save}>Save changes</button>
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            onClick={onClick}
+                        >
+                            Close
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={save}
+                        >
+                            Save changes
+                        </button>
                     </div>
                 </div>
             </ReactModal>
